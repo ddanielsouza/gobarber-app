d89c41955234aa88f7e9424da33d3731651a3b1c
Daniel Souza <daniell.oliveirra@gmail.com>
2020-09-13 21:55:15 -0300

    Validaçao dos fomularios

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,12 +5,14 @@ import {
   Platform,
   View,
   TextInput,
+  Alert,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import {
   Container,
   Title,
@@ -22,13 +24,59 @@ import {
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+interface SignInFormDate {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
-  const handleSignIn = useCallback((data: object) => {
-    console.log('Ok', data);
+
+  const handleSignIn = useCallback(async (data: SignInFormDate): Promise<
+    void
+  > => {
+    const messageRequired = (fieldDescription: string) =>
+      `${fieldDescription} é obrigatório(a)`;
+
+    const messageInvalid = (fieldDescription: string) =>
+      `${fieldDescription} está inválido(a), confira os dados preenchidos`;
+
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required(messageRequired('E-mail'))
+          .email(messageInvalid('E-mail')),
+
+        password: Yup.string().required(messageRequired('Senha')),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // await signIn({
+      //   email: data.email,
+      //   password: data.password,
+      // });
+
+      // history.push('/dashboard');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      } else {
+        Alert.alert(
+          'Error autenticação',
+          'Erro ao fazer login, verifique os dados inseridos',
+        );
+      }
+    }
   }, []);
 
   return (
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,22 +1,78 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  TextInput,
+  Alert,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
+import * as Yup from 'yup';
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignUp: React.FC = () => {
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const emailInputRef = useRef<TextInput>(null);
-  const handleSignUp = useCallback((data: object) => {
-    console.log('Ok', data);
+  interface SignUpFormDate {
+    email: string;
+    password: string;
+    name: string;
+  }
+
+  const handleSignUp = useCallback(async (data: SignUpFormDate): Promise<
+    void
+  > => {
+    const messageRequired = (fieldDescription: string) =>
+      `${fieldDescription} é obrigatório(a)`;
+
+    const messageInvalid = (fieldDescription: string) =>
+      `${fieldDescription} está inválido(a), confira os dados preenchidos`;
+
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required(messageRequired('Nome')),
+        email: Yup.string()
+          .required(messageRequired('E-mail'))
+          .email(messageInvalid('E-mail')),
+        password: Yup.string().min(
+          6,
+          'Preencha a senha com no mínimo 6 digitos',
+        ),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // await api.post('/users', data);
+
+      // history.push('/');
+    } catch (err) {
+      const { ValidationError } = Yup;
+
+      if (err instanceof ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      } else {
+        Alert.alert(
+          'Erro ao cadastrar',
+          'Verifique os dados preenchidos ou tente mais tarde',
+        );
+      }
+    }
   }, []);
 
   return (
